2a3d8ab1a725597b8df2f0cddd48b007dc13ff15
Mhmtugr <m.ugur_10@hotmail.com>
2025-05-06 15:29:32 +0300

    Add files via upload

--- src/services/cad-ai-service.js
+++ src/services/cad-ai-service.js
@@ -11,7 +11,7 @@
 
 import { ref } from 'vue';
 import { aiService } from './ai-service';
-import { logger } from '@/utils/logger';
+import logger from '@/utils/logger';
 
 class CADAnalysisService {
   constructor() {
